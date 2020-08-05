c56f00a5853f8f74d2196040d9394aa33ac171f7
Joo Hyun Kim <stevekim1109@gmail.com>
2020-08-05 09:38:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  array = [element, ...array];
+  var array = [element, ...array];
   return array;
 }
 
